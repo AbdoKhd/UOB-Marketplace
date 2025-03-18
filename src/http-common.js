01559ab685d7499569fa68c0735fcfa241683afe
abdelrahmankhodr <abdrahman.khodr@vianeos.com>
2025-03-18 23:34:58 +0200

    remove client content from folder

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://192.168.1.179:5000",
+  baseURL: "http://192.168.1.179:5001",
   headers: {
     "Content-Type": "application/json",
   },
